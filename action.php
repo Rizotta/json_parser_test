fe96da77fb18429c9d11c16106aaa564863348f2
Rizotta <50727542+Rizotta@users.noreply.github.com>
2020-06-23 02:37:17 +0300

    Change result from server

--- action.php
+++ action.php
@@ -1,11 +1,13 @@
 <?php
 
 // Функция подключения к БД
+
+// Функция подключения к БД
 function connect() {
 	try {
-		$db = new PDO("mysql:host=localhost;dbname=test;charset=UTF8", "user", "password");
+		$db = new PDO("mysql:host=localhost;dbname=test;charset=UTF8", "mysql", "mysql");
 	} catch (PDOException $e) {
-		die("400 error: " . $e->getMessage() );				// В случае ошибки подключения к БД - выдаём 400 и ошибку
+		die("Ошибка подключения к базе. ");	// В случае ошибки подключения к БД - выдаём 400 и ошибку
 	}
 
 	return $db;
@@ -18,7 +20,8 @@ function check_user( $email ) {
 	$stmt = $pdo->prepare( 'SELECT * from users WHERE email = ?' );		// Подготовка запроса поиска пользователя
 
 	if ( !$stmt->execute([ $email ])) {  					// Что-то пошло не так при выполнении - выдаём 400
-		echo = 400;
+		header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+		echo "Возникла ошибка.";
 
 	} elseif ( $stmt->rowCount() === 0 ) {					// Количество записей = 0, значит пользователь не найден. Создадим
 
@@ -27,15 +30,18 @@ function check_user( $email ) {
 
 		$result =  $stmt->errorCode();						// Проверяем результат последней операции
 		if ( $result == "00000" ) {						// Если результат 00000 - значит всё прошло успешно, выдаём 400
-			echo 200;
+			header($_SERVER["SERVER_PROTOCOL"]." 200 ОК");
+			echo "Пользователь успешно добавлен";
 		} else {								// Если результат не 00000 - значит не успешно, выдаём 400
-			echo 400;
+			header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
+			echo "Возникла ошибка.";
 		}
 
 	} else {									// Если пользователь уже существует - сообщаем
-		echo = 'Такой пользователь есть в БД';
+		header($_SERVER["SERVER_PROTOCOL"]." 200 ОК");
+		echo 'Такой пользователь уже существует.';
 	}
 }
 
 $data = json_decode($_POST['jsonData']);
-check_user( htmlspecialchars( $data->email ) );
+check_user( htmlentities( $data->email , ENT_COMPAT, 'UTF-8' ) );
